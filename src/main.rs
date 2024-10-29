b6a93488db60b2b896bd7b9d7093b3da53723880
Loke Gustafsson <lokegustafsson@pm.me>
2024-10-29 14:10:08 +0100

    ported their template solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+mod model;
+
+use model::{Map, Proposal, Request, Response, Action};
+use std::{cell::Cell, fs, time::Duration};
+use tokio::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let map_name = "Gothenburg";
+    let map: Map =
+        serde_json::from_str(&fs::read_to_string(format!("data/Map-{map_name}.json")).unwrap())
+            .unwrap();
+
+    tokio::runtime::Runtime::new().unwrap().block_on(async {
+        let api = Api::new();
+        let request = Request {
+            map_name: map_name.to_string(),
+            proposals: map
+                .customers
+                .iter()
+                .map(|customer| Proposal {
+                    customer_name: customer.name.to_string(),
+                    months_to_pay_back_loan: map.game_length_in_months,
+                    yearly_interest_rate: 0.05,
+                })
+                .collect(),
+            iterations: vec![
+                map.customers
+                    .iter()
+                    .map(|customer| (
+                        customer.name.clone(),
+                        Action {
+                            type_: "Skip".to_string(),
+                            award: "None".to_string(),
+                        }
+                    ))
+                    .collect();
+                map.game_length_in_months
+            ],
+        };
+        api.call(&request).await
+    });
+}
+
+struct Api {
+    api_key: &'static str,
+    earliest_next_call: Cell<Instant>,
+    client: reqwest::Client,
+}
+impl Api {
+    const API_DELAY: Duration = Duration::from_millis(100);
+    const ENDPOINT: &str = "https://api.considition.com/game";
+
+    fn new() -> Self {
+        let api_key = fs::read_to_string(".api-key")
+            .expect("API KEY in `./.api-key`")
+            .leak()
+            .trim();
+        dbg!(api_key);
+        Self {
+            api_key,
+            earliest_next_call: Cell::new(Instant::now()),
+            client: reqwest::Client::new(),
+        }
+    }
+    async fn call(&self, request: &Request) {
+        tokio::time::sleep_until(self.earliest_next_call.get()).await;
+        self.earliest_next_call
+            .set(Instant::now() + Self::API_DELAY);
+        let resp = self
+            .client
+            .post(Self::ENDPOINT)
+            .json(request)
+            .header("x-api-key", self.api_key)
+            .send()
+            .await
+            .unwrap();
+        dbg!(&resp);
+        dbg!(resp.json::<Response>().await.unwrap());
+    }
 }
---
+++ src/model.rs
@@ -0,0 +1,102 @@
+use serde::{Deserialize, Serialize};
+use std::{cell::Cell, collections::BTreeMap, fs, time::Duration};
+use tokio::time::Instant;
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Map {
+    pub name: String,
+    pub budget: f64,
+    pub game_length_in_months: usize,
+    pub customers: Vec<Customer>,
+}
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Customer {
+    pub name: String,
+    pub loan: Loan,
+    pub personality: String,
+    pub capital: f64,
+    pub income: f64,
+    pub monthly_expenses: f64,
+    pub number_of_kids: f64,
+    pub home_mortgage: f64,
+    pub has_student_loan: bool,
+}
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Loan {
+    pub product: String,
+    pub environmental_impact: f64,
+    pub amount: f64,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Personality {
+    pub happiness_multiplier: f64,
+    pub accepted_min_interest: f64,
+    pub accepted_max_interest: f64,
+    pub living_standard_multiplier: f64,
+}
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Award {
+    pub cost: f64,
+    pub base_happiness: f64,
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(deny_unknown_fields)]
+pub struct Proposal {
+    pub customer_name: String,
+    pub months_to_pay_back_loan: usize,
+    pub yearly_interest_rate: f64,
+}
+#[derive(Debug, Clone, Serialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(deny_unknown_fields)]
+pub struct Action {
+    #[serde(rename = "Type")]
+    pub type_: String,
+    pub award: String,
+}
+#[derive(Debug, Clone, Serialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(deny_unknown_fields)]
+pub struct Request {
+    pub map_name: String,
+    pub proposals: Vec<Proposal>,
+    pub iterations: Vec<BTreeMap<String, Action>>,
+}
+impl Request {
+    fn validate(&self) {
+        todo!()
+    }
+}
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Response {
+    pub achievements_unlocked: Vec<serde_json::Value>,
+    pub game_id: String,
+    pub message: (),
+    pub score: Score,
+}
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Score {
+    pub environmental_impact: f64,
+    #[serde(rename = "happynessScore")]
+    pub happiness_score: f64,
+    pub map_name: String,
+    pub total_profit: f64,
+    pub total_score: f64,
+}
